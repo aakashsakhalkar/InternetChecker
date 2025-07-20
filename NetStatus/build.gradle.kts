7ef463032bf621a4537c93fe26e292c4bd93d603
Aakash Sakhalkar <aakash.sakhalkar@gmail.com>
2025-07-20 19:35:34 +0530

    Initial NetStatus module

--- NetStatus/build.gradle.kts
+++ NetStatus/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.library")
-    id("maven-publish")
 }
 
 android {
@@ -27,37 +26,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
-    publishing {
-        singleVariant("release") {
-            withSourcesJar()
-        }
-    }
 }
+
 dependencies {
-    implementation(libs.annotation.jvm)
-}
-
-// ✅ MOVE publishing.publications block into afterEvaluate
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                groupId = "com.github.aakashsakhalkar"
-                artifactId = "NetStatus"
-                version = "1.0.0"
-                from(components["release"])
-            }
-        }
-        repositories {
-            maven {
-                name = "GithubPackages"
-                url = uri("https://maven.pkg.github.com/aakashsakhalkar/NetStatus")
-                credentials {
-                    username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                    password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
-                }
-            }
-        }
-    }
+    implementation("androidx.annotation:annotation:1.7.1")
 }
